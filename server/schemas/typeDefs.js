7c43db28fdc087b568c2217bd001565125fb27e9
Alex Ostrowski <a.ostrowski.11@gmail.com>
2020-10-18 15:21:15 -0500

    added index and typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,54 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+    input Book {
+        bookAuthorsArray : [String]
+        description : String
+        title : String
+        bookId : String
+        image : String
+        link : String
+    }
+    type Book {
+        bookId : Int
+        authors : [String]
+        description : String
+        title : String
+        image : String
+        link : String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+
+ 
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: Book) : User
+        removeBook(bookId: String!) : User
+    }
+
+
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
